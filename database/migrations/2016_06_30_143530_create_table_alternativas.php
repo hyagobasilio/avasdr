8758d44b9588c3a23b0fd7bc4833692ffbd548e9
Hyago Henrique <hyagohba@gmail.com>
2016-07-13 08:43:33 -0300

    Alterando o nome da migration

--- database/migrations/2016_06_30_143530_create_table_alternativas.php
+++ database/migrations/2016_06_30_143530_create_table_alternativas.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTableRespostas extends Migration
+class CreateTableAlternativas extends Migration
 {
     /**
      * Run the migrations.
