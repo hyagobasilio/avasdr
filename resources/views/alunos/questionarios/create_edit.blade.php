e4321977e1bd7880d3465412de6bb7d68c69da47
Hyago Henrique <hyagohba@gmail.com>
2016-09-04 22:03:39 -0300

    Perguntar antes de entregar a prova

--- resources/views/alunos/questionarios/create_edit.blade.php
+++ resources/views/alunos/questionarios/create_edit.blade.php
@@ -1,87 +1,88 @@
 @extends('layouts.docente.template')
 @section('content')
-    <div class="col-md-12 col-sm-12 col-xs-12">
-                <div class="x_panel">
-                  <div class="x_title">
-                    <h2>Questões <small> todas</small></h2>
-                    <ul class="nav navbar-right panel_toolbox">
-                      <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-                      </li>
-                    </ul>
-                    <div class="clearfix"></div>
-                  </div>
-                  <div class="x_content">
-                    <form class="form-horizontal form-label-left" method="post"
-                        action=" {{ url('docente/questionario/create') }}">
-                        <input type="hidden" name="_token" value="{{{ csrf_token() }}}" />
-                        <div class="row">
-                            <!-- Descrição-->
-                            <div class="col-md-4 col-sm-4 col-xs-4 form-group {{ $errors->has('descricao') ? 'bad' : '' }}">
-                                <label for="descricao">Descrição</label>
-                                <input type="text" name="descricao" value="{{ old('descricao')}}" class="form-control">
-                                <ul class="parsley-errors-list filled" id="parsley-id-5">
-                                    <li class="parsley-required">{!! $errors->first('descricao') !!}</li>
-                                </ul>
-                            </div>
-                            
-                            <!-- Turma -->
-                            <div class="col-md-2 col-sm-2 col-xs-2 form-group {{ $errors->has('turma_id') ? 'bad' : '' }}">
-                                <label for="turma_id">Turma</label>
-                                {!! Form::select('turma_id', $turmas, null, ['class' => 'form-control']) !!}
-                                <ul class="parsley-errors-list filled" id="parsley-id-5">
-                                    <li class="parsley-required">{!! $errors->first('turma_id') !!}</li>
-                                </ul>
-                            </div>
-                            
-                            <!-- Vigência-->
-                            <div class="col-md-2 col-sm-2 col-xs-2 form-group {{ $errors->has('vigencia') ? 'bad' : '' }}">
-                                <label for="vigencia">Vigência</label>
-                                <input type="text" name="vigencia" value="{{ old('vigencia')}}"  class="form-control data">
-                                <ul class="parsley-errors-list filled" id="parsley-id-5">
-                                    <li class="parsley-required">{!! $errors->first('vigencia') !!}</li>
-                                </ul>
-                            </div>
-                            
-                            <!-- Vigência-->
-                            <div class="col-md-12 col-sm-12 col-xs-12 ">
-                                <button type="submit" class="btn btn-success">Cadastrar</button>
-                            </div>
-                        </div>
-                    </form>
-                    <table class="table table-striped">
-                      <thead>
-                        <tr>
-                          <th>#</th>
-                          <th>Questão</th>
-                          <th>Capacidade</th>
-                          <th>Elemento de Competência</th>
-                          <th>Objetos de Conhecimento</th>
-                          <th>Dificuldade</th>
-                          <th>Ações</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        @foreach($questoes as $temp)
-                        <tr>
-                          <th scope="row">{{ $temp->questao->id }}</th>
-                          <td>{{ $temp->questao->questao }}</td>
-                          <td>{{ $temp->questao->capacidade }}</td>
-                          <td>{{ $temp->questao->elemento_competencia }}</td>
-                          <td>{{ $temp->questao->obj_conhecimento }}</td>
-                          <td>{{ $temp->questao->dificuldade }}</td>
-                          <td><a href="{{ url('docente/questaotemp/'.$temp->id.'/delete')}}" class="btn"><i class="fa fa-trash"></i></a></td>
-                        </tr>
-                        @endforeach
-                      </tbody>
-                    </table>
-                      
-                    <div class="box-tools pull-right">
-                        {{ $questoes->appends(Request::except('page'))->render() }}
-                    </div>
 
-                  </div>
-                </div>
+<div class="col-md-12 col-sm-12 col-xs-12">
+  <div class="x_panel">
+    <div class="x_title">
+      <h2>Questões <small> todas</small></h2>
+      <ul class="nav navbar-right panel_toolbox">
+        <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+        </li>
+      </ul>
+      <div class="clearfix"></div>
+    </div>
+    <div class="x_content">
+      <form class="form-horizontal form-label-left" method="post"
+          action=" {{ url('docente/questionario/create') }}">
+          <input type="hidden" name="_token" value="{{{ csrf_token() }}}" />
+          <div class="row">
+              <!-- Descrição-->
+              <div class="col-md-4 col-sm-4 col-xs-4 form-group {{ $errors->has('descricao') ? 'bad' : '' }}">
+                  <label for="descricao">Descrição</label>
+                  <input type="text" name="descricao" value="{{ old('descricao')}}" class="form-control">
+                  <ul class="parsley-errors-list filled" id="parsley-id-5">
+                      <li class="parsley-required">{!! $errors->first('descricao') !!}</li>
+                  </ul>
               </div>
+              
+              <!-- Turma -->
+              <div class="col-md-2 col-sm-2 col-xs-2 form-group {{ $errors->has('turma_id') ? 'bad' : '' }}">
+                  <label for="turma_id">Turma</label>
+                  {!! Form::select('turma_id', $turmas, null, ['class' => 'form-control']) !!}
+                  <ul class="parsley-errors-list filled" id="parsley-id-5">
+                      <li class="parsley-required">{!! $errors->first('turma_id') !!}</li>
+                  </ul>
+              </div>
+              
+              <!-- Vigência-->
+              <div class="col-md-2 col-sm-2 col-xs-2 form-group {{ $errors->has('vigencia') ? 'bad' : '' }}">
+                  <label for="vigencia">Vigência</label>
+                  <input type="text" name="vigencia" value="{{ old('vigencia')}}"  class="form-control data">
+                  <ul class="parsley-errors-list filled" id="parsley-id-5">
+                      <li class="parsley-required">{!! $errors->first('vigencia') !!}</li>
+                  </ul>
+              </div>
+              
+              <!-- Vigência-->
+              <div class="col-md-12 col-sm-12 col-xs-12 ">
+                  <button type="submit" class="btn btn-success">Cadastrar</button>
+              </div>
+          </div>
+      </form>
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Questão</th>
+            <th>Capacidade</th>
+            <th>Elemento de Competência</th>
+            <th>Objetos de Conhecimento</th>
+            <th>Dificuldade</th>
+            <th>Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          @foreach($questoes as $temp)
+          <tr>
+            <th scope="row">{{ $temp->questao->id }}</th>
+            <td>{{ $temp->questao->questao }}</td>
+            <td>{{ $temp->questao->capacidade }}</td>
+            <td>{{ $temp->questao->elemento_competencia }}</td>
+            <td>{{ $temp->questao->obj_conhecimento }}</td>
+            <td>{{ $temp->questao->dificuldade }}</td>
+            <td><a href="{{ url('docente/questaotemp/'.$temp->id.'/delete')}}" class="btn"><i class="fa fa-trash"></i></a></td>
+          </tr>
+          @endforeach
+        </tbody>
+      </table>
+        
+      <div class="box-tools pull-right">
+          {{ $questoes->appends(Request::except('page'))->render() }}
+      </div>
+
+    </div>
+  </div>
+</div>
 @endsection
 
 @section('script')
--- resources/views/alunos/questionarios/questionario.blade.php
+++ resources/views/alunos/questionarios/questionario.blade.php
@@ -1,63 +1,93 @@
 @extends('layouts.aluno.template')
 @section('content')
-<div class="col-md-12 col-sm-12 col-xs-12">
-    <div class="x_panel">
-        <div class="x_title">
-            <h2> {{ $questionario->descricao }} </h2>
-            <ul class="nav navbar-right panel_toolbox">
-                <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-                </li>
-            </ul>
-          <div class="clearfix"></div>
-        </div>
-        <div class="x_content">
-            <form class="form-horizontal form-label-left" method="post"
-                action=" {{ url('aluno/questionario/responder') }}">
-                <input type="hidden" name="_token" value="{{{ csrf_token() }}}" />
-                <input type="hidden" name="questionario_id" value="{{{ $questionario->id }}}" />
-                
-                <div class="row">
-                    
-                    <div class="dashboard-widget-content">
 
-                    <ul class="list-unstyled timeline widget">
-                    @foreach($questoes as $q)
-                      <li>
-                        <div class="block">
-                          <div class="block_content">
-                            <h2 class="title">
-                                <a>{!! $q->questao->questao !!}</a>
-                            </h2>
-                            <div class="byline">
-                            <!-- <span>13 hours ago</span> by <a>Jane Smith</a>-->
-                            </div>
-                              <p class="excerpt">{{ $q->questao->comando }}</p>
-                            <div class="form-group">
-                            @foreach($q->questao->alternativas as $alternativa)
-                            <div class="checkbox">
-                                <label>
-                                    <input type="radio" name="{{ $alternativa->questao_id }}" class="" value="{{ $alternativa->id }}"> {{ $alternativa->descricao }}
-                                </label>
-                            </div>
-                            
-                            @endforeach
-                            </div>
-                          </div>
-                        </div>
-                      </li>
-                    @endforeach
-                    
-                    </ul>
-                  </div>
-                            
-                    <!-- Vigência-->
-                    <div class="col-md-12 col-sm-12 col-xs-12 ">
-                        <button type="submit" class="btn btn-success">Cadastrar</button>
-                    </div>
-                </div>
-            </form>
-        </div> <!-- x_content -->
+<div class="modal fade modal-small" tabindex="-1" role="dialog" aria-hidden="true">
+  <div class="modal-dialog modal-sm">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span>
+        </button>
+        <h4 class="modal-title" id="myModalLabelSmall">Entregar a Prova</h4>
+      </div>
+      <div class="modal-body modal-body-small">
+        Deseja entregar a prova?
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>
+        <button type="button" class="btn btn-primary modal-btn-entregar">Entregar</button>
+      </div>
+
     </div>
+  </div>
+</div>
+<!-- /modals -->
+<div class="col-md-12 col-sm-12 col-xs-12">
+  <div class="x_panel">
+    <div class="x_title">
+      <h2> {{ $questionario->descricao }} </h2>
+      <ul class="nav navbar-right panel_toolbox">
+        <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+        </li>
+      </ul>
+      <div class="clearfix"></div>
+    </div>
+    <div class="x_content">
+      <form id="form-avaliacao" class="form-horizontal form-label-left" method="post"
+        action=" {{ url('aluno/questionario/responder') }}">
+        <input type="hidden" name="_token" value="{{{ csrf_token() }}}" />
+        <input type="hidden" name="questionario_id" value="{{{ $questionario->id }}}" />
+        
+        <div class="row">
+          <div class="dashboard-widget-content">
+            <ul class="list-unstyled timeline widget">
+              @foreach($questoes as $q)
+              <li>
+                <div class="block">
+                  <div class="block_content">
+                    <h2 class="title">
+                      <a>{!! $q->questao->questao !!}</a>
+                    </h2>
+                    <div class="byline">
+                      <!-- <span>13 hours ago</span> by <a>Jane Smith</a>-->
+                    </div>
+                    <p class="excerpt">{{ $q->questao->comando }}</p>
+                    <div class="form-group">
+                      @foreach($q->questao->alternativas as $alternativa)
+                      <div class="checkbox">
+                          <label>
+                              <input type="radio" name="{{ $alternativa->questao_id }}" class="" value="{{ $alternativa->id }}"> {{ $alternativa->descricao }}
+                          </label>
+                      </div>
+                    @endforeach
+                    </div>
+                  </div>
+                </div>
+              </li>
+              @endforeach
+            </ul>
+          </div>
+                        
+          <!-- Vigência-->
+          <div class="col-md-12 col-sm-12 col-xs-12 ">
+              <a class="btn btn-success btn-entregar">Entregar</a>
+          </div>
+        </div>
+      </form>
+    </div> <!-- x_content -->
+  </div>
 </div>
 @endsection
 
+@section('script')
+<script>
+  $(document).ready(function(){
+
+    $('.btn-entregar').click(function(){
+      $('.modal-small').modal();
+    });
+    $('.modal-btn-entregar').click(function(){
+      $('#form-avaliacao').submit();
+    });
+  });
+</script>
+@endsection
